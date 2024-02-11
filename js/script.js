e45b8cf1da82fec0422bee2cc4997db8caa7e1c2
David Gonzalez <elavincho@gmail.com>
2024-02-11 00:44:20 -0300

    Primer paso a la app

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 
 /* Funcion para cambiar la imagen principal del producto*/
-function imgSlider(anything){
-    document.querySelector('.productos').src = anything;
+function imgSlider(cambiarImg){
+    document.querySelector('.productos').src = cambiarImg;
   }
+/* Fin Funcion para cambiar la imagen principal del producto*/
 
+/* Buscador de productos*/
+
+document.addEventListener('keyup', e => {
+  if(e.target.matches('#buscador')) {
+    document.querySelectorAll('.articulos').forEach(articulo => {
+      articulo.textContent.toLowerCase().includes(e.target.value)
+      ? articulo.classList.remove('filtro')
+      : articulo.classList.add('filtro')
+    })
+  }
+})
+
+/* Fin Buscador de productos*/
